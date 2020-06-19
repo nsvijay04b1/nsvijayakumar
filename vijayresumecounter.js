47f2c561bbbbf3eb403dbedd5552874a6bf3d1fc
VIJAYAKUMAR <nsvijayakumar@gmail.com>
2020-06-19 10:54:54 +0300

    new apiwateway

--- vijayresumecounter.js
+++ vijayresumecounter.js
@@ -1,15 +1,20 @@
 //Get the visitors element on the page
 
-var xhttp = new XMLHttpRequest();
+const xhttp = new XMLHttpRequest();
+
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
     // Typical action to be performed when the document is ready:
     document.getElementById("visits").innerHTML = xhttp.responseText;
-  }
+   }
 };
+xhttp.open('GET', 'https://cl9x7rqpag.execute-api.eu-west-1.amazonaws.com/Prod/count');
+xhttp.setRequestHeader('X-PINGOTHER', 'pingpong');
+xhttp.setRequestHeader('Access-Control-Allow-Origin', 'nsvijayakumar.com');
+xhttp.setRequestHeader('Content-Type', 'application/xml');
 xhttp.open(
   "GET",
-  "https://cl9x7rqpag.execute-api.eu-west-1.amazonaws.com/Prod/count",
+  "https://47cfysczq6.execute-api.eu-west-1.amazonaws.com/Prod/count",
   true
 );
 xhttp.send();
